c0b8446a19fe25c7572afbc7715533753ec7e299
Alessandro Elias <alessandro.elias@eposnow.com>
2022-09-15 17:30:56 -0300

    Add id_rsa id

--- env.sh
+++ env.sh
@@ -5,4 +5,4 @@ SSH_AGENT_PATH="/run/user/$(id -u)/ssh-agent"
 # WHERE:
 #  filename2pass is the file name at $HOME dir with the password only
 #  PATH_FILE_NAME is absolute path to the private ssh key
-SSH_KEYS=".ecdsa-m3coolAtxps_m3cool:${HOME}/.ssh/id_ecdsa,.alessandro_eliasAteposnow_com:${HOME}/.ssh/eposnow/id_ecdsa"
+SSH_KEYS=".rsa-m3coolAtxps_m3cool:${HOME}/.ssh/id_rsa,.ecdsa-m3coolAtxps_m3cool:${HOME}/.ssh/id_ecdsa,.alessandro_eliasAteposnow_com:${HOME}/.ssh/eposnow/id_ecdsa"
